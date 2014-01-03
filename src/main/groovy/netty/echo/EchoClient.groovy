4b03c1ef300c4dace1af31f45a8e10a7b780762d
Mario Garcia <mario.ggar@gmail.com>
2014-01-03 02:01:24 +0100

    Added EchoServer example

--- src/main/groovy/netty/echo/EchoClient.groovy
+++ src/main/groovy/netty/echo/EchoClient.groovy
@@ -2,21 +2,22 @@ package netty.echo
 
 import java.net.InetSocketAddress
 
-import io.netty.bootstrap.ServerBootstrap
+import io.netty.bootstrap.Bootstrap
 
-import io.netty.channel.ChannelFuture
 import io.netty.channel.EventLoopGroup
+import io.netty.channel.ChannelFuture
 import io.netty.channel.ChannelInitializer
 
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
-import io.netty.channel.socket.nio.NioServerSocketChannel
-
+import io.netty.channel.socket.nio.NioSocketChannel
 /**
+ * EchoClient.groovy
  *
  */
-class EchoServer {
+class EchoClient {
 
+    String host
     int port
 
     void start() {
@@ -25,20 +26,20 @@ class EchoServer {
 
         try {
 
-            ServerBootstrap bootstrap = new ServerBootstrap()
+            Bootstrap bootstrap = new Bootstrap()
 
             bootstrap.
                 group(group).
-                channel(NioServerSocketChannel).
-                localAddress(new InetSocketAddress(port)).
-                childHandler(new ChannelInitializer<SocketChannel>() {
+                channel(NioSocketChannel).
+                remoteAddress(new InetSocketAddress(host, port)).
+                handler(new ChannelInitializer<SocketChannel>() {
                     void initChannel(SocketChannel channel) {
-                        channel.pipeline().addLast(new EchoServerHandler())
+                        channel.pipeline().
+                            addLast(new EchoClientHandler())
                     }
                 })
 
-            ChannelFuture future = bootstrap.bind().sync()
-            println("${EchoServer.simpleName} listening on ${future.channel().localAddress()}")
+            ChannelFuture future = bootstrap.connect().sync()
             future.channel().closeFuture().sync()
 
         } finally {
@@ -49,8 +50,9 @@ class EchoServer {
     }
 
     static void main(args) {
-        new EchoServer(port: 8080).start()
+        new EchoClient(host: '127.0.0.1', port: 8080).start()
     }
+
 
 }
 
---
+++ src/main/groovy/netty/echo/EchoClientHandler.groovy
@@ -0,0 +1,33 @@
+package netty.echo
+
+import io.netty.util.CharsetUtil
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.SimpleChannelInboundHandler
+/**
+ * EchoClientHandler.groovy
+ *
+ */
+@Sharable
+class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    void channelActive(ChannelHandlerContext context) {
+        context.write(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8))
+    }
+
+    void channelRead0(ChannelHandlerContext context, ByteBuf data) {
+        def message = ByteBufUtil.hexDump(data.readBytes(data.readableBytes()))
+
+        println "Client received: $message"
+    }
+
+    void exceptionCaught(ChannelHandlerContext context, Throwable cause) {
+        cause.printStacktrace()
+        context.close()
+    }
+
+}
+
+
