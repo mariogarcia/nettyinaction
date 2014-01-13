cc37c71b1933351c47040e32fb4086fa786a37b3
Mario Garcia <mario.ggar@gmail.com>
2014-01-13 23:47:41 +0100

    Still figuring out why can't generate more than one class with the same file

--- clojure/src/main/clojure/netty/echo/server.clj
+++ clojure/src/main/clojure/netty/echo/server.clj
@@ -1,39 +1,48 @@
 (ns netty.echo.server
 
-    (:gen-class
-     :name netty.echo.server.EchoServerHandler
-     :extends io.netty.channel.ChannelInboundHandlerAdapter
-     :prefix "_adapter_"
-     :methods [
-       [channelRead [io.netty.channel.ChannelHandlerContext Object] Void]
-       [channelReadComplete [io.netty.channel.ChannelHandlerContext] Void]
-       [exceptionCaught [io.netty.channel.ChannelHandlerContext Throwable] Void]
-     ]
-    )
-
   (:import
       (netty.util NettyUtil)
+      (java.net InetSocketAddress)
+      (io.netty.channel
+        ChannelFuture
+        ChannelFutureListener
+        ChannelInitializer
+        ChannelHandlerContext
+        ChannelInboundHandlerAdapter
+        EventLoopGroup)
       (io.netty.bootstrap ServerBootstrap)
-      (io.netty.channel ChannelFuture EventLoopGroup ChannelInitializer)
+      (io.netty.buffer Unpooled)
+      (io.netty.util CharsetUtil)
       (io.netty.channel.nio NioEventLoopGroup)
       (io.netty.channel.socket SocketChannel)
-      (io.netty.channel.ChannelInboundHandlerAdapter)
-      (io.netty.channel.ChannelHandlerContext)
       (io.netty.channel.socket.nio NioServerSocketChannel)
-      (java.net InetSocketAddress)
     )
 
+  (:gen-class
+    :name netty.echo.server.EchoServerHandler
+    :extends io.netty.channel.ChannelInboundHandlerAdapter
+    :prefix "adapter-"
+    :methods [
+       [channelRead [io.netty.channel.ChannelHandlerContext Object] Void]
+       [channelReadComplete [io.netty.channel.ChannelHandlerContext] Void]
+       [exceptionCaught [io.netty.channel.ChannelHandlerContext java.lang.Throwable] Void]
+    ]
+  )
 
 )
 
-(defn _adapter_channelRead [context data] (
+(defn adapter-channelRead [context data]
+    "Reading the content of the bytes"
+    ())
 
-))
+(defn adapter-channelReadComplete [context]
+    "Once the message has been read then we flush the output"
+    (let [once-flush
+            (.writeAndFlush context Unpooled/EMPTY_BUFFER)]
+            (.addListener once-flush ChannelFutureListener/CLOSE)))
 
-(defn _adapter_channelReadComplete [context] (
+(defn adapter-exceptionCaught [context throwable]
+    "If anything goes wrong maybe we should handle it somehow"
+    (.printStackTrace throwable)
+    (.close context))
 
-))
-
-(defn _adapter_exceptionCaught [context throwable] (
-
-))
