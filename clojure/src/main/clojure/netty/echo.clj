969a37a936c0a7902a91a625c13ba658301110a6
Mario Garcia <mario.ggar@gmail.com>
2014-01-16 00:36:42 +0100

    Andrey Refactoring

--- clojure/src/main/clojure/netty/echo.clj
+++ clojure/src/main/clojure/netty/echo.clj
@@ -1,36 +1,29 @@
 (ns netty.echo
+  (:import (netty.util NettyUtil)
+           (io.netty.bootstrap ServerBootstrap)
+           (io.netty.channel ChannelFuture EventLoopGroup ChannelInitializer)
+           (io.netty.channel.nio NioEventLoopGroup)
+           (io.netty.channel.socket SocketChannel)
+           (io.netty.channel.socket.nio NioServerSocketChannel)
+           (java.net InetSocketAddress))
+  (:require [netty.echo.server :refer [channel-adapter]])
 
-    (:gen-class
-     :name netty.echo.EchoServer
-     :prefix "-")
-
-    (:import
-      (netty.util NettyUtil)
-      (io.netty.bootstrap ServerBootstrap)
-      (io.netty.channel ChannelFuture EventLoopGroup ChannelInitializer)
-      (io.netty.channel.nio NioEventLoopGroup)
-      (io.netty.channel.socket SocketChannel)
-      (io.netty.channel.socket.nio NioServerSocketChannel)
-      (java.net InetSocketAddress)
-    )
-
-)
+    (:gen-class))
 
 (defn -main []
   "This example builds an echo server with Netty"
-  (let [group (NioEventLoopGroup.)]
-    (let [bootstrap (ServerBootstrap.)]
+  (let [group     (NioEventLoopGroup.)
+        bootstrap (ServerBootstrap.)
+        handler   (channel-adapter)]
 
-      (doto (NettyUtil.)
-        (.group bootstrap group)
-        (.channel bootstrap NioServerSocketChannel)
-        (.localAddress bootstrap (InetSocketAddress. 8080))
-      )
+    (.channelReadComplete handler d df)
+    (doto (NettyUtil.)
+      (.group bootstrap group)
+      (.channel bootstrap NioServerSocketChannel)
+      (.localAddress bootstrap (InetSocketAddress. 8080)))
 
-      (let [fut (.sync (.bind bootstrap))]
-        (.sync (.closeFuture (.channel fut))))
+    (let [fut (.sync (.bind bootstrap))]
+      (.sync (.closeFuture (.channel fut))))))
 
-    )
-  )
-)
+
 
--- clojure/src/main/clojure/netty/echo/server.clj
+++ clojure/src/main/clojure/netty/echo/server.clj
@@ -1,48 +1,28 @@
 (ns netty.echo.server
+  (:import (netty.util NettyUtil)
+           (java.net InetSocketAddress)
+           (io.netty.channel ChannelFuture
+                             ChannelFutureListener
+                             ChannelInitializer
+                             ChannelHandlerContext
+                             ChannelInboundHandlerAdapter
+                             EventLoopGroup)
+           (io.netty.bootstrap ServerBootstrap)
+           (io.netty.buffer Unpooled)
+           (io.netty.util CharsetUtil)
+           (io.netty.channel.nio NioEventLoopGroup)
+           (io.netty.channel.socket SocketChannel)
+           (io.netty.channel.socket.nio NioServerSocketChannel))
 
-  (:import
-      (netty.util NettyUtil)
-      (java.net InetSocketAddress)
-      (io.netty.channel
-        ChannelFuture
-        ChannelFutureListener
-        ChannelInitializer
-        ChannelHandlerContext
-        ChannelInboundHandlerAdapter
-        EventLoopGroup)
-      (io.netty.bootstrap ServerBootstrap)
-      (io.netty.buffer Unpooled)
-      (io.netty.util CharsetUtil)
-      (io.netty.channel.nio NioEventLoopGroup)
-      (io.netty.channel.socket SocketChannel)
-      (io.netty.channel.socket.nio NioServerSocketChannel)
-    )
+(defn channel-adapter
+  []
+  (proxy [] [ChannelInboundHandlerAdapter]
+    (channelRead [context data])
 
-  (:gen-class
-    :name netty.echo.server.EchoServerHandler
-    :extends io.netty.channel.ChannelInboundHandlerAdapter
-    :prefix "adapter-"
-    :methods [
-       [channelRead [io.netty.channel.ChannelHandlerContext Object] Void]
-       [channelReadComplete [io.netty.channel.ChannelHandlerContext] Void]
-       [exceptionCaught [io.netty.channel.ChannelHandlerContext java.lang.Throwable] Void]
-    ]
-  )
+    (channelReadComplete [context]
+      (let [once-flush (.writeAndFlush context Unpooled/EMPTY_BUFFER)]
+        (.addListener once-flush ChannelFutureListener/CLOSE)))
 
-)
-
-(defn adapter-channelRead [context data]
-    "Reading the content of the bytes"
-    ())
-
-(defn adapter-channelReadComplete [context]
-    "Once the message has been read then we flush the output"
-    (let [once-flush
-            (.writeAndFlush context Unpooled/EMPTY_BUFFER)]
-            (.addListener once-flush ChannelFutureListener/CLOSE)))
-
-(defn adapter-exceptionCaught [context throwable]
-    "If anything goes wrong maybe we should handle it somehow"
-    (.printStackTrace throwable)
-    (.close context))
-
+    (exceptionCaught [context throwable]
+      (.printStackTrace throwable)
+      (.close context)))))
